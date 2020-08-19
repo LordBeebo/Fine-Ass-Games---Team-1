01174b13f4ece327dcd7bda3bce69b897acd5d87
Josh K <jkiv189@gmail.com>
2020-08-19 00:36:26 -0500

    Restructure and comment

--- clicker.js
+++ clicker.js
@@ -1,36 +1,58 @@
+//Initialize global variables at zero
 let bankQty = 0;
 let cashFlow = 0;
 
+//Reach out to HTML for buttons
+const counter = document.getElementById("counter");
+const mainButton = document.getElementById("main-click");
+const childButton = document.getElementById("child");
+const factoryButton = document.getElementById("factory");
+
+//"Tiers" class allows for easy construction of new tiers
+class Tiers {
+    currentNum = 0
+
+    constructor(baseCost, baseDPS){
+        this.baseCost = baseCost
+        this.baseDPS = baseDPS
+    }
+
+    purchase() {
+        if (bankQty >= this.baseCost) {
+            bankQty -= this.baseCost;
+            cashFlow += this.baseDPS;
+            counterUpdate();
+        }
+    }
+}
+
+//Create instances of "Tiers"
+const child = new Tiers(50, 2);
+const factory = new Tiers(200, 10);
+
+//Generic counter updater
+function counterUpdate() {
+    counter.innerHTML = 
+    `<p> Money: \$${bankQty} </p>
+    <p> Dollars per second: \$${cashFlow}`  
+}
+
+//Main money clicker function
 function clicker() {
     bankQty ++;
-    console.log(bankQty);
-    counter.innerHTML = 
-    `<p> Money: \$${bankQty} </p>
-    <p> Dollars per second: \$${cashFlow}`;
+    counterUpdate();
 }
 
+//Action to be taken every turn (one second)
 function turn() {
     bankQty += cashFlow;
-    console.log(cashFlow, bankQty);
-    counter.innerHTML = 
-    `<p> Money: \$${bankQty} </p>
-    <p> Dollars per second: \$${cashFlow}`
+    counterUpdate()
 }
 
-setInterval(turn, 1000);
-
-const mainButton = document.getElementById("main-click");
+//Assign functions to buttons
 mainButton.addEventListener('click', clicker);
+childButton.addEventListener('click', child.purchase.bind(child));
+factoryButton.addEventListener('click', factory.purchase.bind(factory));
 
-const counter = document.getElementById("counter");
-counter.innerHTML = 
-    `<p> Money: \$${bankQty} </p>
-    <p> Dollars per second: \$${cashFlow}`;
-
-const childButton = document.getElementById("child");
-childButton.addEventListener('click', () => {
-    if (bankQty >= 100){
-        cashFlow += 5;
-        bankQty -= 100;
-    }
-});
+//Start the 1Hz actions
+setInterval(turn, 1000);
